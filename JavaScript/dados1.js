8320279867e065570ab812eed872d15da43280c6
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-19 06:59:55 -0300

    JavaScript: Variaveis

---
+++ JavaScript/dados1.js
@@ -0,0 +1,17 @@
+let nome = "Caneta";
+var quantidade = 10;
+var preco = 3.50;
+var imposto = 1.5;
+const precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome);
+
+teste = 123;
+console.log(teste);
