96049acac09c677af8482db90016e0a390060553
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-19 07:20:21 -0300

    JavaScript: Tipos de dados

---
+++ JavaScript/dados3.js
@@ -0,0 +1,9 @@
+let idade = 31;
+let salario = 4330.00;
+let nome = "Guilherme";
+let estaChovendo = true; // ou false
+
+console.log(typeof idade);
+console.log(typeof salario);
+console.log(typeof nome);
+console.log(typeof estaChovendo);
