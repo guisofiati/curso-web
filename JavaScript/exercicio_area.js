c40f3b59366e315b80dc68f01cbd86d53ca61314
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-19 07:31:23 -0300

    JavaScript: Area circunferencia

---
+++ JavaScript/exercicio_area.js
@@ -0,0 +1,8 @@
+const PI = 3.141592;
+let raio = 10;
+let area = PI * raio * raio;
+
+console.log("A área da circunferência é: " + area + "m2");
+
+area = Math.PI * raio * raio
+console.log("A área da circunferência é: " + area + "m2");
