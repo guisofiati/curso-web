1b2ac78d0446f6044e1a6fcfa8e9389ba38c2102
Guilherme Sofiati Fantin <gui_sf@hotmail.com>
2021-11-30 07:36:59 -0300

    JavaScript: Objetos

---
+++ JavaScript/objetos.js
@@ -0,0 +1,25 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra'
+prod1.preco = 4000.0
+prod1.desconto = 0.4
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 85.00
+}
+
+console.log(prod2)
+
+const prod3 = {
+    nome: 'T-Shirt Kevin',
+    preco: 50.00,
+    cores: {
+        cor1: 'Azul',
+        cor2: 'Vermelho',
+        cor3: 'Amarelo'
+    }
+}
+
+console.log(prod3)
